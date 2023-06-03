a5f649e7f4ce47cd6af62af38fbf2ae908f242a9
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-06-03 16:58:40 -0300

    fix: test commit

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -4,7 +4,7 @@ export class RemoteAuthentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient
-  ) {}
+  ) {} 
 
   async auth(): Promise<void> {
     await this.httpPostClient.post(this.url)
