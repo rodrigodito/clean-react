e961b800f9ed6b39b2ee5f115922bee83aaaee41
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-06-04 13:56:36 -0300

    refactor: add generic params to HttpPostClient

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,14 +1,14 @@
 import { type HttpPostParams, type HttpPostClient } from '@/data/protocols/http/http-post-client'
 import { HttpStatusCode, type HttpResponse } from '@/data/protocols/http/http-response'
 
-export class HttpPostClientSpy implements HttpPostClient {
+export class HttpPostClientSpy<T, R>implements HttpPostClient<T, R> {
   url?: string
-  body?: object
-  response: HttpResponse = {
+  body?: T
+  response: HttpResponse<R> = {
     statusCode: HttpStatusCode.ok
   }
 
-  async post (params: HttpPostParams): Promise<HttpResponse> {
+  async post (params: HttpPostParams<T>): Promise<HttpResponse<R>> {
     this.url = params.url
     this.body = params.body
     return await Promise.resolve(this.response)
