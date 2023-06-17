d51f2441746e4d59b077e054d5c76536ad767bfe
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-06-17 11:27:40 -0300

    chore: add testing library

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { Router } from '@/presentation/components/Router/Router'
+import '@/presentation/styles/global.scss'
 
 ReactDom.render(
   <Router />,
