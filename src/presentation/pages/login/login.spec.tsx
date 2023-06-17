745f5c7ad115dd9f3e97a6f4632ecfbd32531435
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-06-17 12:50:17 -0300

    test: ensure Login start with inputs with require state/

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -5,10 +5,19 @@ import { Login } from './login'
 describe('Login Component', () => {
   test('Should start with initial state', () => {
     const { getByTestId } = render(<Login />)
+
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
 
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+
+    const emailInputStatus = getByTestId('email-status')
+    expect(emailInputStatus.title).toBe('Campo obrigatório')
+    expect(emailInputStatus.textContent).toBe('🔴')
+
+    const passwordInputStatus = getByTestId('password-status')
+    expect(passwordInputStatus.title).toBe('Campo obrigatório')
+    expect(passwordInputStatus.textContent).toBe('🔴')
   })
 })
