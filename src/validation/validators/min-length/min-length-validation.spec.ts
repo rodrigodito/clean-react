8cc22f8a7451b6d127e705f52cbc54029104454b
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-06-24 13:29:10 -0300

    test: ensure MinLengthValidation return falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -8,4 +8,11 @@ describe('MinLengthValidation', () => {
 
     expect(error).toEqual(new InvalidFiledError())
   })
+
+  test('Should return falsy if value is valid', () => {
+    const sut = new MinLengthValidation('password', 5)
+    const error = sut.validate('12345')
+
+    expect(error).toBeFalsy()
+  })
 })
