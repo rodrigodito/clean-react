25352fc9e5bd649037559dafef6e28e17e142101
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-06-24 13:25:49 -0300

    test: ensure MinLengthValidation return error if value is invalid

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,11 @@
+import { InvalidFiledError } from '@/validation/errors'
+import { MinLengthValidation } from './min-length-validation'
+
+describe('MinLengthValidation', () => {
+  test('Should return error if value is invalid', () => {
+    const sut = new MinLengthValidation('password', 5)
+    const error = sut.validate('123')
+
+    expect(error).toEqual(new InvalidFiledError())
+  })
+})
