5bc785eb260ef4a8a3985d2740f9c56d1f11df54
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-06-24 12:22:30 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,17 @@
 import { EmailValidation } from '@/validation/validators/email/email-validation'
 import { InvalidFiledError } from '@/validation/errors'
+import { faker } from '@faker-js/faker'
 
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('')
+    const sut = new EmailValidation(faker.database.column())
+    const error = sut.validate(faker.lorem.word())
     expect(error).toEqual(new InvalidFiledError())
+  })
+
+  test('Should return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.database.column())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
