a4e32a2c357419e7c986e91e0c43c4a2b9dffcb8
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-06-24 11:48:38 -0300

    test: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,10 +1,17 @@
 import { RequiredFieldError } from '@/validation/errors'
 import { RequiredFieldValidation } from '@/validation/required-field/required-field-validation'
+import { faker } from '@faker-js/faker'
 
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Should return falsy if field is not empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.lorem.word())
+    expect(error).toBeFalsy()
   })
 })
