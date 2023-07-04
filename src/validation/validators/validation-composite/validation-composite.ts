575bf34e354c58c83c67b841a1c238d17abfdcd9
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-07-03 22:13:27 -0300

    refactor: change ValidationComposite to use a static build method to instance

--- src/validation/validators/validation-composite/validation-composite.ts
+++ src/validation/validators/validation-composite/validation-composite.ts
@@ -2,7 +2,11 @@ import { type Validation } from '@/presentation/protocols/validation'
 import { type FieldValidation } from '@/validation/protocols/field-validation'
 
 export class ValidationComposite implements Validation {
-  constructor (private readonly validators: FieldValidation[]) {}
+  private constructor (private readonly validators: FieldValidation[]) {}
+
+  static build (FieldValidations: FieldValidation[]): ValidationComposite {
+    return new ValidationComposite(FieldValidations)
+  }
 
   validate (fieldName: string, fieldValue: string): string {
     const validators = this.validators.filter(validator => validator.field === fieldName)
