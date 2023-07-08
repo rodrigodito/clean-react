605ce1f81b74d352b45a2949a6872a59f059a492
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-07-08 11:08:43 -0300

    refactor: move validation test folder to correct place

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,6 +1,6 @@
 import { faker } from '@faker-js/faker'
 import { ValidationComposite } from './validation-composite'
-import { FieldValidationSpy } from '@/validation/validators/test/mock-field-validation'
+import { FieldValidationSpy } from '@/validation/test'
 
 type SutTypes = {
   sut: ValidationComposite
