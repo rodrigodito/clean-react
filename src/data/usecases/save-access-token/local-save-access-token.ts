69e227a5acb2be5d89cfc51d50f828ab3c770462
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-07-16 13:05:18 -0300

    feat: ensure LocalSaveAccessToken calls SetStorage with corret values

---
+++ src/data/usecases/save-access-token/local-save-access-token.ts
@@ -0,0 +1,9 @@
+import { type SetStorage } from '@/data/protocols/cache/set-storage'
+import { type SaveAccessToken } from '@/domain/usecases/save-access-token'
+
+export class LocalSaveAccessToken implements SaveAccessToken {
+  constructor (private readonly setStorage: SetStorage) {}
+  async save (accessToken: string): Promise<void> {
+    await this.setStorage.set('accessToken', accessToken)
+  }
+}
