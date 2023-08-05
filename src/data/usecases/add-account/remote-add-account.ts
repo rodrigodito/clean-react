f51c3e0bb58fb4fecb4b3dc2ffa2cfc92cf5413d
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-08-05 11:55:02 -0300

    feat: ensure RemoteAddAccount calls HttpPostClient with correct URL

---
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -0,0 +1,19 @@
+import { type HttpPostClient } from '@/data/protocols/http'
+import { type AccountModel } from '@/domain/models'
+import { type AddAccount, type AddAccountParams } from '@/domain/usecases'
+
+export class RemoteAddAccount implements AddAccount {
+  constructor (
+    private readonly url: string,
+    private readonly httpPostClient: HttpPostClient<AddAccountParams, AccountModel>
+  ) {}
+
+  async add (params: AddAccountParams): Promise<AccountModel> {
+    await this.httpPostClient.post({
+      url: this.url,
+      body: params
+    })
+
+    return { jwt: '' }
+  }
+}
