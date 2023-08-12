0d9a1005e2782484f344e61ac2befb19e3c49b67
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-08-12 11:14:50 -0300

    test: ensure SignUp change email status to error if validation fails

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -37,7 +37,7 @@ describe('SignUp Component', () => {
     Helper.testChildCount(sut, 'error-wrap', 0)
     Helper.testButtonIsDisabled(sut, 'submit', true)
     Helper.testStatusForField(sut, 'name', fakeValidationError)
-    Helper.testStatusForField(sut, 'email', 'Campo obrigatório')
+    Helper.testStatusForField(sut, 'email', fakeValidationError)
     Helper.testStatusForField(sut, 'password', 'Campo obrigatório')
     Helper.testStatusForField(sut, 'passwordConfirmation', 'Campo obrigatório')
   })
@@ -47,5 +47,19 @@ describe('SignUp Component', () => {
     const { sut } = makeSut({ validationError: fakeValidationError })
     Helper.populateField(sut, 'name')
     Helper.testStatusForField(sut, 'name', fakeValidationError)
+  })
+
+  test('Should show name error if Validation fails', () => {
+    const fakeValidationError = faker.word.words()
+    const { sut } = makeSut({ validationError: fakeValidationError })
+    Helper.populateField(sut, 'name')
+    Helper.testStatusForField(sut, 'name', fakeValidationError)
+  })
+
+  test('Should show email error if Validation fails', () => {
+    const fakeValidationError = faker.word.words()
+    const { sut } = makeSut({ validationError: fakeValidationError })
+    Helper.populateField(sut, 'email')
+    Helper.testStatusForField(sut, 'email', fakeValidationError)
   })
 })
