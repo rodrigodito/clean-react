96aff6ffb7a9a6d38d427691010a182e1416f3a9
Rodrigo Benedito <rodrigo.carvalho@kabum.com.br>
2023-08-12 11:18:25 -0300

    feat: ensure SignUp change password status to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -16,9 +16,10 @@ export function SignUp ({ validation }: Props) {
     isLoading: false,
     name: '',
     email: '',
+    password: '',
     nameError: '',
     emailError: '',
-    passwordError: 'Campo obrigatório',
+    passwordError: '',
     passwordConfirmationError: 'Campo obrigatório',
     mainError: ''
   })
@@ -27,9 +28,10 @@ export function SignUp ({ validation }: Props) {
     setState({
       ...state,
       nameError: validation.validate('name', state.name),
-      emailError: validation.validate('name', state.email)
+      emailError: validation.validate('email', state.email),
+      passwordError: validation.validate('password', state.password)
     })
-  }, [state.name, state.email])
+  }, [state.name, state.email, state.password])
 
   return (
     <div className={Styles.signup}>
